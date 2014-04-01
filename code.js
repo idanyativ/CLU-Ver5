df17214a38047fcb38fa818d904a43013b91cae0
idanyativ <idan_yativ@walla.co.il>
2014-04-01 22:50:56 +0300

    before review

--- code.js
+++ code.js
@@ -10,7 +10,10 @@ var length = 0;
 var value = null;
 var image = null;
 var carousels = {};
+var history;
 var firstTap = true;
+var removedItem = 0;
+var numberOfSearch = 0;
 /**
  * 
  * @param inputValue - Get the value from the user
@@ -51,7 +54,7 @@ function getCLU(inputValue) {
  */
 function sendValueToServer(value) {
     console.log("Get Clue About " + value);
-//    preventDefault();
+    // e.preventDefault();
     $.ajax({
         type: "GET",
         url: 'http://noanimrodidan.milab.idc.ac.il/?q=' + value,
@@ -69,6 +72,9 @@ function sendValueToServer(value) {
                 resultsInString = JSON.parse(JSON.stringify(incomeResults));
                 threeResults = cutResults(resultsInString);
                 image = incomeResults.imageURL;
+                history[numberOfSearch] = value;
+                numberOfSearch++;
+                console.log(history);
                 buildPage2(resultsInString, image);
 
             }
@@ -113,8 +119,8 @@ function localSend(val) {
  * @param {type} value
  * @returns {undefined}
  */
-function goToWiki(value) {
-    window.open("http://en.wikipedia.org/wiki/" + value, "_self");
+function goToWiki() {
+    window.open("http://en.wikipedia.org/wiki/" + getInputValue(), "_self");
 }
 
 function getInputValue() {
@@ -158,8 +164,11 @@ function getInputValue() {
 //;
 
 function buildPage2(results, image)
-{
+{   
     $.mobile.changePage('#resultPage');
+    count = 0;
+    numberOfSearch = 0;
+    removedItem = 0;
     $('#resultSearch').val(getInputValue());
     $('#resultImages').css({"background-image": "url" + "(" + image + ")", "background-repeat": "no-repeat", "background-size": "100% 100%"});
     showOnlyValue();
@@ -173,18 +182,19 @@ function buildPage2(results, image)
         for (var i = 0; i < 3; i++) {
             addElement(i);
         }
-        console.log("count"  + count);
+        console.log("count" + count);
     }
     $('#resultPage').html();
     $('#resultPage').trigger("create");//refreashing dynamically
     $('#resultPage a').on('click', function(e) {
         e.preventDefault();
     });
-};
+}
+;
 
 function getContext(i) {
     if (firstTap) {
-        $('#lineup').html(result.results[i].value + " - " + result.results[i].context).css({"font-size": "100%"});
+        $('#lineup').html("<b>" + result.results[i].value + "</b><br>" + result.results[i].context).css({"font-size": "100%"});
         $('#lineup').css({top: '70%'});
         $('#lineup').animate({height: '30%'});
         $('#lineup').attr("onClick", "showOnlyValue()");
@@ -222,10 +232,9 @@ function cutResults(res) {
 }
 
 function removeFromList(el) {
-    //console.log("removed number" + index);
-    console.log("asdasd" + el);
     $(el).remove();
-    $('#resultPage').trigger("refresh");//refreashing dynamically
+    removedItem++;
+    $('#resultPage').trigger("refresh");
 }
 
 function randomPage() {
@@ -256,44 +265,57 @@ function startOver() {
 
 function getAnotherValue(toRemove) {
     // no more words to show
-    if (count == length-1) {
-        alert("no more words");
-        removeFromList(toRemove);
+//    if (count == length-1) {
+    if (count == 6) {
+        if (removedItem == 5) {
+//            alert("asdasd");
+            removeFromList(toRemove);
+//            alert("finish");
+            addWikiElement();
+        } else {
+//            alert("no more words");
+//            console.log("tooo: " + removedItem);
+            removeFromList(toRemove);
+        }
     } else {
-       // alert("id" + $(this));
+        // alert("id" + $(this));
         console.log("this" + $(this).value);
         removeFromList(toRemove);
         addElement(count);
     }
 }
 
-function addElement(nextSeq){
+function addElement(nextSeq) {
     var holder = document.getElementById("resultContent");
-            $(holder).append("<div id=\carousel" + nextSeq + ">" + "</div>");
-            var resultsList = document.getElementById("carousel" + nextSeq);
-            $(resultsList).append("<ul id=\listBricks" + nextSeq + ">");
-            $("#listBricks" + nextSeq).css({"height": "50px", "text-align": "center", "border": "solid 1px black"});
-            var resultsList2 = document.getElementById("listBricks" + nextSeq);
-            $(resultsList2).append("<li id=\"pane1\">Get New CLU</li>");
-            $("#listBricks" + nextSeq + " > " + "#pane1").css({"background": "rgb(72,182,233)", "background-image": "url(searchListItem.png)", "background-repeat": "no-repeat", "background-size": " 20% 100%", "height": "100%", "font-weight": "bold", "font-style": "italic", "font-size": "150%"});
-            $(resultsList2).append("<li id=\"pane2\"><a>" + result.results[nextSeq].value + "</a></li>");
-            $("#listBricks" + nextSeq + " > " + "#pane2").css({"height": "100%", "color": "grey", "font-weight": "bold", "font-style": "italic", "font-size": "150%"});
-            $(resultsList2).append("<li id=\"pane3\">Throw This CLU</li>");
-            $("#listBricks" + nextSeq + " > " + "#pane3").css({"background": "rgb(250,20,20)", "background-image": "url(delListItem.png)", "background-repeat": "no-repeat", "background-size": "20% 100%", "height": "100%", "font-weight": "bold", "font-style": "italic", "font-size": "150%"});
-            $(resultsList).append("</ul>");
-            carousels[nextSeq] = new Carousel("#carousel" + nextSeq);
-            
-//            $("#carousel" + nextSeq).css({"padding": "0","margin":"0","position":"relative"});
-//            $("#carousel" + nextSeq + " ul").css({"padding": "0","margin":"0","position":"relative"});
-//            $("#carousel" + nextSeq + " li").css({"padding": "0","margin":"0","position":"relative"});            
-//            $("#carousel" + nextSeq).css({"overflow": "hidden","background": "white","width": "100%","-webkit-backface-visibility": "hidden","-webkit-transform": "translate3d(0,0,0) scale3d(1,1,1)","-webkit-transform-style": "preserve-3d"});
-//            $("ul.animate").css({"-webkit-transition": "all .3s","-moz-transition": "all .3s","-o-transition": "all .3s","transition": "all .3s"});
-//             $("#carousel" + nextSeq + " ul").css({"-webkit-transform": "translate3d(0%,0,0) scale3d(1,1,1)","-moz-transform": "translate3d(0%,0,0) scale3d(1,1,1)","-ms-transform": "translate3d(0%,0,0) scale3d(1,1,1)","-o-transform": "translate3d(0%,0,0) scale3d(1,1,1)"," transform": "translate3d(0%,0,0) scale3d(1,1,1)","overflow": "hidden","-webkit-backface-visibility": "hidden","-webkit-transform-style": "preserve-3d","-webkit-box-shadow": "0 0 20px rgba(0,0,0,.2)","box-shadow": "0 0 20px rgba(0,0,0,.2)","position": "relative"});            $("#carousel" + nextSeq + " ul").css({"overflow": "hidden","-webkit-backface-visibility": "hidden","-webkit-transform-style": "preserve-3d","-webkit-box-shadow": "0 0 20px rgba(0,0,0,.2)","box-shadow": "0 0 20px rgba(0,0,0,.2)","position": "relative"});
-//             $("#carousel" + nextSeq + " li").css({"float": "left","overflow": "hidden","-webkit-transform-style": "preserve-3d"});
-    
-    
-    
+    $(holder).append("<div id=\carousel" + nextSeq + ">" + "</div>");
+    var resultsList = document.getElementById("carousel" + nextSeq);
+    $(resultsList).append("<ul id=\listBricks" + nextSeq + ">");
+    $("#listBricks" + nextSeq).css({"height": "50px", "text-align": "center", "border": "solid 1px black"});
+    var resultsList2 = document.getElementById("listBricks" + nextSeq);
+    $(resultsList2).append("<li id=\"pane1\">Get New CLU</li>");
+    $("#listBricks" + nextSeq + " > " + "#pane1").css({"background": "rgb(72,182,233)", "background-position": "right", "background-image": "url(searchListItem.png)", "background-repeat": "no-repeat", "background-size": " 20% 100%", "height": "100%", "font-weight": "bold", "font-style": "italic", "font-size": "150%"});
+    $(resultsList2).append("<li id=\"pane2\"><a>" + result.results[nextSeq].value + "</a></li>");
+    $("#listBricks" + nextSeq + " > " + "#pane2").css({"height": "100%", "color": "grey", "font-style": "italic", "font-size": "150%", "font-family": "'Lato', sans-serif"});
+    $(resultsList2).append("<li id=\"pane3\">Throw This CLU</li>");
+    $("#listBricks" + nextSeq + " > " + "#pane3").css({"background": "rgb(250,20,20)", "background-image": "url(delListItemRed.png)", "background-repeat": "no-repeat", "background-size": "20% 100%", "height": "100%", "font-weight": "bold", "font-style": "italic", "font-size": "150%"});
+    $(resultsList).append("</ul>");
+    carousels[nextSeq] = new Carousel("#carousel" + nextSeq);
     carousels[nextSeq].init();
-            console.log("next number " +nextSeq );
-            count++;
-        } 
+    console.log("next number " + nextSeq);
+    count++;
+} 
+
+function addWikiElement(){
+    $('#lineup').html("<b>" + "Need Another Clu?!" + "</b>").css({"font-size": "200%","text-align":"center","line-height":"5"});
+    $('#lineup').css({top: '0%'});
+    $('#lineup').animate({height: '100%'});
+//    $('#lineup').attr("onClick", "showOnlyValue()");
+    var holder = document.getElementById("resultContent");
+    $(holder).append("<div onClick=\"randomPage()\">" + "Get Random Clu"  + "</div>");
+    $(holder).css({"text-align":"center","background":"red","margin-top":"5%","width":"70%","margin-left":"15%","font-size":"130%"});
+    }
+//     $(holder).append("<ul data-role=\"listview\"");
+//    $(holder).append("<li id=\random"+" onClick=\"randomPage()\">"+"Get Random" + "</li>").css({"text-align":"center","background":"red"});
+//    $(holder).append("<li id=\wiki"+" onClick=\"gotowiki()\">"+"Go To Wiki" + "</li>").css({"text-align":"center","background":"blue"});
+//    $(holder).append("<li id=\startOver"+" onClick=\"startOver()\">"+"Start Over" + "</li").css({"text-align":"center","background":"pink"});
+//}   $(holder).append("</ul>");
